9968bdbd40495f94035ec44e261b2e62bbf19728
liangyongrui <liangyongrui@kuaishou.com>
2022-11-24 14:18:00 +0800

    .

---
+++ os4/src/syscall/process.rs
@@ -0,0 +1,77 @@
+//! Process management syscalls
+
+use crate::task::{
+    current_task_info, exit_current_and_run_next, suspend_current_and_run_next,
+    translated_current_ptr, TaskStatus,
+};
+use crate::timer::get_time_us;
+use crate::{config::MAX_SYSCALL_NUM, mm::translated_ptr, task::current_user_token};
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TimeVal {
+    pub sec: usize,
+    pub usec: usize,
+}
+
+#[derive(Clone, Copy)]
+pub struct TaskInfo {
+    pub status: TaskStatus,
+    pub syscall_times: [u32; MAX_SYSCALL_NUM],
+    pub time: usize,
+}
+
+pub fn sys_exit(exit_code: i32) -> ! {
+    info!("[kernel] Application exited with code {}", exit_code);
+    exit_current_and_run_next();
+    panic!("Unreachable in sys_exit!");
+}
+
+/// current task gives up resources for other tasks
+pub fn sys_yield() -> isize {
+    suspend_current_and_run_next();
+    0
+}
+
+// YOUR JOB: 引入虚地址后重写 sys_get_time
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    let us = get_time_us();
+
+    unsafe {
+        *translated_current_ptr(ts) = TimeVal {
+            sec: us / 1_000_000,
+            usec: us % 1_000_000,
+        };
+    }
+    0
+}
+
+// CLUE: 从 ch4 开始不再对调度算法进行测试~
+pub fn sys_set_priority(_prio: isize) -> isize {
+    -1
+}
+
+/// start 需要映射的虚存起始地址，要求按页对齐
+/// len 映射字节长度，可以为 0
+/// port：第 0 位表示是否可读，第 1 位表示是否可写，第 2 位表示是否可执行。其他位无效且必须为 0
+/// # Error
+/// 1. start 没有按页大小对齐
+/// 1. port & !0x7 != 0 (port 其余位必须为0)
+/// 1. port & 0x7 = 0 (这样的内存无意义)
+/// 1. [start, start + len) 中存在已经被映射的页
+/// 1. 物理内存不足
+pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
+    -1
+}
+
+pub fn sys_munmap(_start: usize, _len: usize) -> isize {
+    -1
+}
+
+// YOUR JOB: 引入虚地址后重写 sys_task_info
+pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
+    unsafe {
+        *translated_current_ptr(ti) = current_task_info();
+    }
+    0
+}
